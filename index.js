a8f03e7b69245b3f7f48a93ad025033a6b38c4a7
raghava32 <34959104+raghava32@users.noreply.github.com>
2018-10-08 22:56:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
@@ -13,14 +12,12 @@ server.use(bodyParser.urlencoded({
 server.use(bodyParser.json());
 
 server.post('/svempdetails', (req, res) => {
-	
-	console.log('ragh');
-	console.write('raga');
 
     const movieToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.movie ? req.body.result.parameters.movie : 'The Godfather';
-    const reqUrl = encodeURI(`http://10.242.212.80:8000/sap/opu/odata/SAP/ZMDG_FIAA_ASSET_SRV/CRequestSet?$format=json`);
-
-    http.get(reqUrl, (responseFromAPI) => {
+    const reqUrl = encodeURI(`http://svsoftappdev.sandv.biz:8000/sap/opu/odata/SAP/ZMDG_FIAA_ASSET_SRV/CRequestSet?$format=json`);
+    http.get(reqUrl,{
+    headers: {'Authorization': 'Basic cm5pbW1hbGE6Um5pbW1hbGEyMDE5'}
+}, (responseFromAPI) => {
         let completeResponse = '';
         responseFromAPI.on('data', (chunk) => {
             completeResponse += chunk;
@@ -29,23 +26,22 @@ server.post('/svempdetails', (req, res) => {
         responseFromAPI.on('end', () => {
             var JSONObj = JSON.parse(completeResponse);
         
-		
             let botResponse ;
             /// = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
          var JSONObj = JSON.parse(completeResponse);
 	     
           if (JSONObj.d.results.length > 0) 
           {
-            botResponse = 'Employee Data :'
+            botResponse = "CR Data :"
 			
             for (var i = 0; i < JSONObj.d.results.length; i++) 
             {
-            botResponse += ' ';
-              botResponse +=  'ChangeReq:-'+JSONObj.d.results[i].UsmdCrequest +' ,UsmdCreqType:- '+ JSONObj.d.results[i].UsmdCreqType +' .';  
-		        				 
+            botResponse += " ";
+              botResponse +=  "UsmdCrequest:- "+JSONObj.d.results[i].UsmdCrequest +"UsmdCreqType:- "+ JSONObj.d.results[i].UsmdCreqType +" .";  
+			  
 	           }
           } 
-            console.log(botResponse) 
+             
             return res.json({
                 speech: botResponse,//dataToSend,
                 displayText: botResponse,
@@ -63,5 +59,5 @@ server.post('/svempdetails', (req, res) => {
 
 
 server.listen((process.env.PORT || 3000), () => {
-    console.log('Server is up and running...');
+    console.log("Server is up and running...");
 });
