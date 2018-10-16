e1b322495d13b3bc8dcd01552f86a2bffd801722
raghava32 <34959104+raghava32@users.noreply.github.com>
2018-10-16 12:52:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,29 +17,29 @@ server.post('/assetdata', (req, res) => {
 	let cr_str;
  // const cr_sel = req.body.queryResult.queryText;
 	const cr_sel = req.body.queryResult.queryText.any;
-    /*   switch (cr_sel)  
+       switch (cr_sel)  
     { 
        case 'pending cr': 
-		    cr_str = "'" + cr_sel + "'"
+	    cr_str = "'" + cr_sel + "'";
            botResponse = 'Pending Changerequest : ';
            break; 
        case 'priority cr': 
-		    cr_str = "'" + cr_sel + "'"
+		    cr_str = "'" + cr_sel + "'";
            botResponse = 'Priority Changerequest : ';
            break; 
        case 'latest cr': 
-		    cr_str = "'" + cr_sel + "'"
+		    cr_str = "'" + cr_sel + "'";
            botResponse = 'Latest Changerequest : ';
            break; 
 	   case 'over due cr': 
-		    cr_str = "'" + cr_sel + "'"
+		    cr_str = "'" + cr_sel + "'";
            botResponse = 'Over Due cr Changerequest : ';
            break; 
        default: 
            botResponse = 'Changerequest : ';
     }  
 
-	*/	
+
 		
    
     const reqUrl = encodeURI(`http://80.227.35.222:50000/sap/opu/odata/SAP/ZMDG_TAXNMY_BOT_SRV/CRequestSet?$filter= Zfval eq 'pend'&$format=json`);
