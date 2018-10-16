af781dbb34df5e9c8ea1c4dd06c57b0cbacc7c83
raghava32 <34959104+raghava32@users.noreply.github.com>
2018-10-16 11:39:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,111 +11,61 @@ server.use(bodyParser.urlencoded({
 
 server.use(bodyParser.json());
 
-server.post('/asset', (req, res) => {
-let text;
- //const movieToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.movie ? req.body.result.parameters.movie : 'The Godfather';
-    const reqUrl = encodeURI('http://80.227.35.222:50000/sap/opu/odata/SAP/ZMM_EMP_SRV_01/CRequestSet?$format=json');
+server.post('/assetdata', (req, res) => {
+
+   let botResponse ;
+  const cr_sel = req.body.queryResult.queryText;
+       switch (cr_sel)  
+    { 
+       case 'pending cr': 
+           botResponse = 'Pending Changerequest : ';
+           break; 
+       case 'priority cr': 
+           botResponse = 'Priority Changerequest : ';
+           break; 
+       case 'latest cr': 
+           botResponse = 'Latest Changerequest : ';
+           break; 
+	   case 'over due cr': 
+           botResponse = 'Over Due cr Changerequest : ';
+           break; 
+       default: 
+           botResponse = 'Changerequest : ';
+    }  
+
+		
+		
+   
+    const reqUrl = encodeURI(`http://80.227.35.222:50000/sap/opu/odata/SAP/ZMDG_TAXNMY_BOT_SRV/CRequestSet?$filter= Zfval eq ${cr_sel}&$format=json`);
+	///sap/opu/odata/SAP/ZMDG_TAXNMY_BOT_SRV/CRequestSet?$filter= Zfval eq 'pending cr'&$format=json
+	//const reqUrl = encodeURI(`http://80.227.35.222:50000/sap/opu/odata/SAP/ZMDG_TAXNMY_BOT_SRV/CRequestSet?$filter= Zfval eq 'pending cr'&$format=json`);
+		
+	console.log(reqUrl);
     http.get(reqUrl, (responseFromAPI) => {
         let completeResponse = '';
         responseFromAPI.on('data', (chunk) => {
             completeResponse += chunk;
+			
             
         });
         responseFromAPI.on('end', () => {
-            var JSONObj = JSON.parse(completeResponse);
-
-			//////////////////////
-		
-		//return an array of objects according to key, value, or key and value matching
-function getObjects(obj, key, val) {
-    var objects = [];
-    for (var i in obj) {
-        if (!obj.hasOwnProperty(i)) continue;
-        if (typeof obj[i] == 'object') {
-            objects = objects.concat(getObjects(obj[i], key, val));    
-        } else 
-        //if key matches and value matches or if key matches and value is not passed (eliminating the case where key matches but passed value does not)
-        if (i == key && obj[i] == val || i == key && val == '') { //
-            objects.push(obj);
-        } else if (obj[i] == val && key == ''){
-            //only add if the object is not already in the array
-            if (objects.lastIndexOf(obj) == -1){
-                objects.push(obj);
-            }
-        }
-    }
-    return objects;
-}
-
-//return an array of values that match on a certain key
-function getValues(obj, key) {
-    var objects = [];
-    for (var i in obj) {
-        if (!obj.hasOwnProperty(i)) continue;
-        if (typeof obj[i] == 'object') {
-            objects = objects.concat(getValues(obj[i], key));
-        } else if (i == key) {
-            objects.push(obj[i]);
-        }
-    }
-    return objects;
-}
-
-//return an array of keys that match on a certain value
-function getKeys(obj, val) {
-    var objects = [];
-    for (var i in obj) {
-        if (!obj.hasOwnProperty(i)) continue;
-        if (typeof obj[i] == 'object') {
-            objects = objects.concat(getKeys(obj[i], val));
-        } else if (obj[i] == val) {
-            objects.push(i);
-        }
-    }
-    return objects;
-}
-
-
-var js = JSON.parse(completeResponse);
-
-//grabbing objects that match some key and value in JSON
-//console.log(getObjects(js, 'Empid','5'));
-//console.log(getObjects(js, 'UsmdCreqText','demo sv check notes'));
-
-
-let cr_response;
-let reqparams;
-
-// const parameters = req.body.queryResult.parameters;
-//var cr = parameters['number'];
-const parameters = req.body.queryResult.queryText;
-
-if  ( parameters == "Latest CR");{
-	
-   cr_response =  getObjects(js, 'UsmdCreqStatus','04');
-}
-
-
-
-//var name = cr_response[1];
-//returns 1 object where a key names ID has the value SGML
-
-
-
-console.log(cr_response.length);
-  
-
- text = "USMD CRequest :- ";
-for (let l = 0; l < cr_response.length; l++) {
-
- text += "  " + cr_response[l].UsmdCrequest + ". ";
-   //text += " USMD CRequestSet :- " + cr_response[l].UsmdCrequest + ", ";
-}  
-
-
+            var JSONObj = JSON.parse(completeResponse);        
+	     
+          if (JSONObj.d.results.length > 0) 
+          {
+			 
+            		
+            for (var i = 0; i < JSONObj.d.results.length; i++) 
+            {
+            botResponse += " ";
+              botResponse +=  JSONObj.d.results[i].UsmdCrequest +" "+" .";  
+			  
+	           }
+          } 
+             console.log(botResponse);
             return res.json({
-                speech: text,//dataToSend,
-                displayText: text,
+                speech: botResponse,//dataToSend,
+                displayText: botResponse,
                 source: 'webhook-echo-sample'
             });
         });
