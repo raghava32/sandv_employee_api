a4996911e9f0f3d75c3b6733a166b2fcac10d4ef
raghava32 <34959104+raghava32@users.noreply.github.com>
2018-10-17 19:39:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ const numb = req.body.result.parameters;
         "items": [
           {
             "simpleResponse": {
-              "textToSpeech": botResponse
+              "Speech": botResponse
             }
           },
           {
