d084270565dcf776870f18cf8366da387641c363
raghava32 <34959104+raghava32@users.noreply.github.com>
2018-10-18 01:09:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ server.post('/assetdata', (req, res) => {
 	let reqUrl = "";
 	let bt_resp = "";
 const numb = req.body.result.parameters;
+const quer = req.body.result.query;	
 	const num = numb["crdata"]
-	//console.log(num);
+	console.log("quer " +quer);
  switch(num) {
 	 case "cr data":
 	 case "change request data":
