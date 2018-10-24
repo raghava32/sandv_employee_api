056f2bd91d76e1d377ca82048a410753a389cc13
raghava32 <34959104+raghava32@users.noreply.github.com>
2018-10-24 18:11:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ console.log("checking quer" + quer); //3418
 	    // for if condition if (num.length > 1) 
 		  //console.log("quer != null checking");
 		
-		 if ((quer != null && !res_query.includes("approve")) || (quer != null && !res_query.includes("reject")) {	  
+		 if ((quer != null && !res_query.includes("approve")) || (quer != null && !res_query.includes("reject"))) {	  
 	             cr_1 = "Material :" + JSONObj.d.Matnr;
 		     cr_1d = "Desc:" + JSONObj.d.Txtmi;
 	             cr_2 =  "Noun :" + JSONObj.d.NounName;
